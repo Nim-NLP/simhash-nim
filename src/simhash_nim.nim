a020e3ec591d950f278aa8bcd10bcccc4765f693
zhoupeng <zhoupeng@xjgreat.com>
2018-09-02 20:47:33 +0800

    o

--- src/simhash_nim.nim
+++ src/simhash_nim.nim
@@ -12,7 +12,7 @@ import parseutils
 import math
 
 const 
-    defualtReg = r"[\w]+"
+    defaultReg = r"[\w]+"
     defaultF = 64
 
 type 
@@ -41,7 +41,7 @@ iterator iterMasks(f:int):tuple[key:int,val:int] =
     for res in 0..<f:
         yield (key:res,val:1 shl res)
 
-proc buildByFeatures( self:var Simhash, features:seq[tuple[k:string,w:int]]) =
+proc buildByFeatures[T]( self:var Simhash, features:T) =
     var 
         v = newSeq[int](self.f)
         h:MD5Digest
@@ -77,29 +77,41 @@ proc numDifferingBits*(a,b:SomeInteger):SomeInteger=
 proc distance(self:Simhash,other:Simhash):int64 =
     result = numDifferingBits(self.value,other.value)
 
-proc initSimhash*(value:string, f=defaultF, reg = defualtReg, hashfunc = toMD5 ) : Simhash =
+proc getFeature[T](features:T):seq[tuple[k:string,w:int]] =
+    for x in features:
+        result.add( (k:x,w:1) )
+
+proc initSimhash*(value:string, f=defaultF, reg = defaultReg, hashfunc = toMD5 ) : Simhash =
     result = Simhash(f:f,reg:re(reg),hashfunc:hashfunc)
     result.buildByText(value)
 
-# proc initSimhash(features:seq[tuple[k:string,w:int]] or openArray[tuple[k:string,w:int]], f = defaultF, reg = defualtReg, hashfunc = toMD5 ) : Simhash =
-#     result = Simhash(f:f,reg:re(reg),hashfunc:hashfunc)
-#     result.build_by_features(features)
+proc initSimhash*(features:seq[tuple[k:string,w:int]] , f = defaultF, reg = defaultReg, hashfunc = toMD5 ) : Simhash =
+    result = Simhash(f:f,reg:re(reg),hashfunc:hashfunc)
+    result.build_by_features(features)
 
-# proc initSimhash(features:seq[string] or openArray[string], f = defaultF, reg = defualtReg, hashfunc = toMD5 ) : Simhash =
-#     result = Simhash(f:f,reg:re(reg),hashfunc:hashfunc)
-#     var 
-#         r:seq[tuple[k:string,w:int]] = @[]
-#     for x in features:
-#         r.add( (k:x,w:1) )
-#     result.build_by_features(r)
+proc initSimhash*(features: openArray[tuple[k:string,w:int]], f = defaultF, reg = defaultReg, hashfunc = toMD5 ) : Simhash =
+    result = Simhash(f:f,reg:re(reg),hashfunc:hashfunc)
+    result.build_by_features(features)
+
+proc initSimhash*(features:seq[string], f = defaultF, reg = defaultReg, hashfunc = toMD5 ) : Simhash =
+    result = Simhash(f:f,reg:re(reg),hashfunc:hashfunc)
+    result.build_by_features(getFeature(features))
+
+proc initSimhash*(features:openArray[string], f = defaultF, reg = defaultReg, hashfunc = toMD5 ) : Simhash =
+    result = Simhash(f:f,reg:re(reg),hashfunc:hashfunc)
+    result.build_by_features(getFeature(features))
 
 when isMainModule:
     let a = 0xDEADBEEF;
     let b = 0xDEADBEAD;
     let expected = 2;
     assert numDifferingBits(a,b) == expected
-    # let sh3 = initSimhash(["aaa", "bbb"])
-    # assert sh3.value ==  57087923692560392
+    let sh3 = initSimhash(["aaa", "bbb"])
+    assert sh3.value == 57087923692560392
+
+    let sh4 = initSimhash([ ("aaa",1), ("bbb",1)])
+    assert sh4.value == 57087923692560392
+
     let sh = initSimhash("How are you? I AM fine. Thanks. And you?")
     let sh2 = initSimhash("How old are you ? :-) i am fine. Thanks. And you?")
     assert sh.distance(sh2) > 0
